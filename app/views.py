9f10f7d3afd2c0057a4007c55cd8f15ee20b5792
stefan <stefan.fouche@jumo.world>
2020-10-17 15:06:21 +0200

    added a template for a new homepage and its endpoint in views

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def home():
    return "hello world!"
+
+@app.route('/home')
+def template():
+    return render_template('home.html')
